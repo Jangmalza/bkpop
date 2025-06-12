1423b6870be7fb68936b6cbcf680de30f0c5e33a
장현우 <>
2025-06-12 17:40:54 +0900

    메뉴바 토글 구현 커밋

--- bkpop-front/src/components/MainCategoryNav.js
+++ bkpop-front/src/components/MainCategoryNav.js
@@ -1,20 +1,111 @@
-// src/components/MainCategoryNav.js
+import { useState } from 'react';
 import './MainCategoryNav.css';
 
 function MainCategoryNav() {
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setDropdownOpen((prev) => !prev);
+  };
+
   return (
     <div className="main-category-nav">
       <div className="nav-inner">
         <div className="category-left">
+          <div className="menu-wrapper">
+            <button className="menu-button" onClick={toggleDropdown}>
+              {dropdownOpen ? '✖' : '≡'}
+            </button>
+
+            {dropdownOpen && (
+              <div className={`dropdown-menu show mega-menu`}>
+                <div className="mega-grid">
+
+                  <div className="mega-column">
+                    <h4>명함</h4>
+                    <ul>
+                      <li>일반지명함</li>
+                      <li>고급지명함</li>
+                      <li>카드명함</li>
+                      <li>하이브리드명함</li>
+                      <li>투명하이브리드명함</li>
+                      <li>에폭시명함</li>
+                      <li>프리컷팅</li>
+                    </ul>
+                    <h4>봉투</h4>
+                    <ul>
+                      <li>대/중/소</li>
+                      <li>규격형</li>
+                      <li>자켓형</li>
+                      <li>안내형</li>
+                      <li>기성형</li>
+                      <li>캘린더봉투</li>
+                    </ul>
+                  </div>
+
+                  <div className="mega-column">
+                    <h4>전단</h4>
+                    <ul>
+                      <li>합판전단</li>
+                      <li>고급전단</li>
+                      <li>랑데뷰합판</li>
+                    </ul>
+                    <h4>스티커</h4>
+                    <ul>
+                      <li>재단형</li>
+                      <li>도무송형</li>
+                      <li>차량스티커</li>
+                      <li>메탈스티커</li>
+                      <li>라벨스티커(롤)</li>
+                    </ul>
+                  </div>
+
+                  <div className="mega-column">
+                    <h4>테스트</h4>
+                    <ul>
+                      <li>테스트</li>
+                      <li>테스트</li>
+                      <li>테스트</li>
+                      <li>테스트</li>
+                      <li>테스트</li>
+                    </ul>
+                  </div>
+
+                  <div className="mega-column">
+                    <h4>홍보물</h4>
+                    <ul>
+                      <li>카탈로그/브로셔</li>
+                      <li>포스터</li>
+                      <li>리플렛</li>
+                      <li>책자</li>
+                      <li>홀더</li>
+                    </ul>
+                  </div>
+
+                  <div className="mega-column">
+                    <h4>홍보물</h4>
+                    <ul>
+                      <li>카탈로그/브로셔</li>
+                      <li>포스터</li>
+                      <li>리플렛</li>
+                      <li>책자</li>
+                      <li>홀더</li>
+                    </ul>
+                  </div>                  
+
+                </div>
+              </div>
+            )}
+          </div>
+
           <ul className="category-list">
-            <button className="menu-button">≡</button>
-            <li>상업인쇄</li>
-            <li>디지털인쇄</li>
-            <li>판촉물</li>
-            <li>패키지</li>
-            <li>기획상품</li>
-            <li>셀프디자인</li>
-            <li>실사출력</li>
+            <li>테스트</li>
+            <li>테스트</li>
+            <li>테스트</li>
+            <li>테스트</li>
+            <li>테스트</li>
+            <li>테스트</li>
+            <li>테스트</li>
           </ul>
         </div>
 
